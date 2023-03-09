2d224e8be809a581a0fab4d49c20065305ae71dc
AleksandraJ <jonoska.aleksandra@yahoo.com>
2023-03-09 15:50:10 -0700

    updated cukes runner

--- src/test/java/com/twilia/runners/CukesRunner.java
+++ src/test/java/com/twilia/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/twilia/step_definitions",
         dryRun = false,
-        tags = "",
+        tags = "@B28G7-221",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
